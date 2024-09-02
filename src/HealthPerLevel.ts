0d47d808c8121cab899caeb47175cbc9eb6db1cf
Andrzej Frysiak <fryciarz7@gmail.com>
2024-09-02 16:32:56 +0200

    adds health per level for PMC

--- src/HealthPerLevel.ts
+++ src/HealthPerLevel.ts
@@ -150,6 +150,11 @@ class HealthPerLevel implements IPreSptLoadMod, IPostDBLoadMod
         {
             bodyPart[key].Health.Maximum =
         preset[key] + (Math.trunc((accountLevel - 1)/this.config.levels_per_increment_PMC)) * this.increasePerLevelPMC[key];
+            if (this.config.health_per_health_skill_level == true && this.pmcHealthSkillLevel)
+            {
+                const healthBonus = Math.floor(this.pmcHealthSkillLevel.Progress / 100);
+                bodyPart[key].Health.Maximum += healthBonus;
+            }
         }
     }
 
